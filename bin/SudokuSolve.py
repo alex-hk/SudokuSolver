569a8a215cae88506c9c4cdd4ca7b300823861fd
alex-hk <alexveseli123@gmail.com>
2017-05-30 21:27:08 -0400

    First commit

---
+++ bin/SudokuSolve.py
@@ -0,0 +1,29 @@
+import os
+
+class SudokuSolver(object):
+    def __init__(self,fname):
+        return
+
+    def solve(self):
+        return
+
+    def checkHor(self):
+        return
+
+    def checkVer(self):
+        return
+
+    def checkNon(self):
+        return
+    
+    def validate(self, fname):
+        return
+
+
+
+dirpath = "./data/"
+solvpath = "./solve/"
+
+for filename in os.listdir('data'):
+    f = open(filename, 'r')
+
