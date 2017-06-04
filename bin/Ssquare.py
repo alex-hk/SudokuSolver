e11d4269b5e32ea7c1302cd7342607c60720d721
alex-hk <alexveseli123@gmail.com>
2017-06-04 17:52:22 -0400

    Commit 060417

---
+++ bin/Ssquare.py
@@ -0,0 +1,26 @@
+class Ssquare(object):
+
+    # pnums: array of possible values for specific square
+    # count: number of values in pnums
+    # actual: actual value for square
+    def __init__(self):
+        self.count = 0
+        self.pnums
+        self.actual = 0
+
+    # Changes actual value if val exists (May not work correctly)
+    def ssActual(self, val):
+        if val:
+            self.actual = val
+        else:
+            return self.actual
+    
+    # Checks if there is only one value in Pnums
+    # If so, value in pnums is actual value
+    def ifActual(self):
+        return self.count == 1
+
+    # Adds value into pnum if it does not exist
+    def addPnum(self, val):
+        if val not in self.pnums:
+            self.pnums.append(val)
--- bin/SudokuSolve.py
+++ bin/SudokuSolve.py
@@ -1,9 +1,20 @@
 import os
+import Ssquare
 
 class SudokuSolver(object):
+
+    # Initialize file, 9x9 array of Ssquare objects
+    # (View Ssquare.py for more info on Ssquare object)
     def __init__(self,fname):
         return
 
+    # Solve puzzle
+    # Steps:
+    # 1. Mark each possible number in each square
+    #   i. If square has only one number, set Ssquare's actual to that number
+    #     1. Check each Ssquare in horizontal, vertical, and 3x3 square
+    #        and remove that number from each
+    # 2. Check 
     def solve(self):
         return
 
