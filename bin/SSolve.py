7160fdd58a0efbab38fa99ab58a0591631c7a3d7
alex-hk <alexveseli123@gmail.com>
2017-06-06 13:07:57 -0400

    More UI/Getting Data

--- bin/SSolve.py
+++ bin/SSolve.py
@@ -6,10 +6,15 @@ class SSolve(object):
 
     # Initialize file, 9x9 array of Ssquare objects
     # (View Ssquare.py for more info on Ssquare object)
-    def __init__(self, filename, speed):
+    def __init__(self, filename, speed, spnums):
         self.file = open(filename)
         self.ssq = [ [Sbox() for row in range(9)] for row in range(9)]
         self.speed = speed
+        self.iterMarks  = 0
+        self.iterSolve  = 0
+        self.iterRemove = 0
+        self.showPnums  = spnums
+        #self.
 
 
     def initSsq(self):
@@ -39,6 +44,18 @@ class SSolve(object):
             print
         print
 
+    def printBoth(self):
+        for list in self.ssq:
+            for box in list:
+                print "{0:d} ".format(box.actual),
+            print "\t",
+            for box2 in list:
+                print box2.pnums,
+                if len(box2.pnums) < 3:
+                    print "\t",
+                print "\t",
+            print
+        print
 
 
     def solve(self):
@@ -64,12 +81,16 @@ class SSolve(object):
                                 self.ssq[row][col].actual = val
                                 del self.ssq[row][col].pnums[:]
                                 self.removeAll(row, col, val)
-                                self.printPuzzle()
+                                if self.showPnums:
+                                    self.printBoth()
+                                else:
+                                    self.printPuzzle()
                                 #self.printPnums()
                                 if self.speed > 0:
                                     time.sleep(self.speed)
                                 else:
-                                    raw_input("Press Enter to continue...")
+                                    if self.speed == 0:
+                                        raw_input("Press Enter to continue...")
             #                    print "NOT SINGLE\n"
                             
                     else:
@@ -92,10 +113,12 @@ class SSolve(object):
         count = 0
         for i in range(0,9):
             if mark:
-          #      print "Checking horizontal if value is single"
+                self.iterSolve+=1
+          #     print "Checking horizontal if value is single"
                 if self.ssq[row][i].actual == 0 and val in self.ssq[row][i].pnums:
                     count+=1
             else:
+                self.iterMarks+=1
                 if self.ssq[row][i].actual == val:
                     return True
         if mark:
@@ -108,10 +131,12 @@ class SSolve(object):
         count=0
         for i in range(0,9):
             if mark:
-         #       print "Checking vertical if value is single"
+                self.iterSolve+=1
+         #      print "Checking vertical if value is single"
                 if self.ssq[i][col].actual == 0 and val in self.ssq[i][col].pnums:
                     count+=1
             else:
+                self.iterMarks+=1
                 if self.ssq[i][col].actual == val:
                     return True
         if mark:
@@ -138,11 +163,13 @@ class SSolve(object):
             for col in range(scol, scol+3):
            #     print "Stuff"
                 if mark is True:
+                    self.iterSolve+=1
                     #print "Checking box if value is single"
                     if self.ssq[row][col].actual == 0 and val in self.ssq[row][col].pnums:
                         #print "Box contains value"
                         count+=1
                 else:
+                    self.iterMarks+=1
                     if self.ssq[row][col].actual == val:
                         return True
         if mark:
@@ -152,36 +179,60 @@ class SSolve(object):
 
     def removeVer(self, col, val):
         for i in range(0,9):
+            self.iterRemove+=1
             if val in self.ssq[i][col].pnums:
-                self.ssq[i][col].pnums.remove(val)
+                self.ssq[i][col].removePnum(val)
 
     def removeHor(self, row, val):
         for i in range(0,9):
+            self.iterRemove+=1
             if val in self.ssq[row][i].pnums:
-                self.ssq[row][i].pnums.remove(val)
+                self.ssq[row][i].removePnum(val)
 
     def removeBox(self, box, val):
         srow = (box/3)*3
         scol = (box%3)*3
         for row in range(srow, srow+3):
             for col in range(scol, scol+3):
+                self.iterRemove+=1
                 if val in self.ssq[row][col].pnums:
-                    self.ssq[row][col].pnums.remove(val)
+                    self.ssq[row][col].removePnum(val)
 
 
-    # Check if done
-    def validate(self, fname):
-        return
+    def validate(self):
+        for row in range(0,9):
+            for col in range(0,9):
+                box  = (row/3)*3 + (col/3)
+                val = self.ssq[row][col].actual
+                if not self.boxValid(box, val) and not self.horValid(row, val) and not self.verValid(col, val):
+                    return False
+        return True
 
-    def Solve(self, row, col):
-        srow = row / 3
-        scol = col / 3
-        box = (srow * 3) + scol
-        if self.sarr[row][col] != '0':
-            for i in range(1,10):
-                return
-                #if checkBox(
+    def boxValid(self, box, val):
+        srow = (box/3)*3
+        scol = (box%3)*3
+        count = 0
+        for row in range(srow, srow+3):
+            for col in range(scol, scol+3):
+                if self.ssq[row][col].actual == val:
+                    count+=1
+        return count == 1
     
+    def horValid(self, row, val):
+        count = 0
+        for i in range(0,9):
+            if self.ssq[row][i].actual == val:
+                count+=1
+        return count == 1
+
+    def verValid(self, col, val):
+        count = 0
+        for i in range(0,9):
+            if self.ssq[i][col].actual == val:
+                count+=1
+        return count == 1
+
+
     def calcMarks(self):
         for row in range(0,9):
             for col in range(0,9):
@@ -192,6 +243,8 @@ class SSolve(object):
                     for val in range(1,10):
                         if not self.checkbox(box, val, False) and not self.checkHor(row, val, False) and not self.checkVer(col, val, False):
                             self.ssq[row][col].addPnum(val)
+
+                        self.iterMarks+=1
 
 
 
--- bin/handler.py
+++ bin/handler.py
@@ -1,19 +1,48 @@
 from SSolve import SSolve
+import time
 
 filename = "data/easy"
 extension = ".txt"
+spnums = False
+speed = -1
+ifspeed = True
 
 print "Welcome to Aleksandar Veselinovic's Sudoku Solver"
 print "The program runs at a specified speed"
-print "1 is one second, .5 is half a second, below 0 is press to continue"
-speed = raw_input("Please enter a speed: ")
+print "1 is one second, .5 is half a second, 0 is press to continue, -1 is off"
+choice = raw_input("Please enter a speed: ")
+if choice:
+    speed = float(choice)
+print "Would you like to show possible numbers? Y/N?"
+choice = raw_input("")
+if choice.lower() == "y":
+    spnums = True
+
 
 for num in range(1,5):
     fname = filename + str(num) + extension
-    x = SSolve(fname, float(speed))
+    x = SSolve(fname, float(speed), spnums)
+    start = time.time()
     x.initSsq()
     x.calcMarks()
     x.solve()
-    raw_input("Press Enter to continue...")
+    end = time.time()
+    siter = x.iterSolve
+    miter = x.iterMarks
+    riter = x.iterRemove
+    titer = siter+miter+riter
+    print "Puzzle done"
+    print "Time to complete: ",end-start
+    print "Iterations to calculate marks: {0:d}".format(siter)
+    print "Iterations to find matches: {0:d}".format(miter)
+    print "Iterations to remove marks: {0:d}".format(riter)
+    print "Total iterations: {0:d}".format(titer)
+    print "Validating if puzzle is correct"
+    time.sleep(1)
+    if x.validate():
+        print "Puzzle is correct"
+    else: print "Puzzle is incorrect"
+
+    raw_input("Press Enter to continue to next puzzle")
 
 print "Thank you for using my program"
